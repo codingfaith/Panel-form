0b3bec15774b6eec026cfb53f772d80254383544
codingfaith <codingfaith158@gmail.com>
2023-10-19 15:03:32 +0200

    Add thank you div,hide form after form submission

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-let yearDropdown = document.getElementById('year')
-
+const yearDropdown = document.getElementById('year')
 // Create an option for each year from 1923 to 2023
 for (let year = 2023; year >= 1923; year--) {
     let option = document.createElement('option')
@@ -7,3 +6,13 @@ for (let year = 2023; year >= 1923; year--) {
     option.text = year
     yearDropdown.appendChild(option)
 }
+//handle form submission
+const form = document.getElementById("form")
+const thanksDiv = document.getElementById("thanks")
+const submitBtn = document.getElementById("submit")
+
+submitBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    thanksDiv.style.display = "block"
+    form.style.display = "none"   
+})
